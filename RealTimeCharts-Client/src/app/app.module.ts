51a04dd54655e9e667b95fd03e16107543356c33
Oliver Arias <Oliver_Arias@claro.com.do>
2019-11-19 08:09:50 -0400

    End

--- RealTimeCharts-Client/src/app/app.module.ts
+++ RealTimeCharts-Client/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ChartsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ RealTimeCharts_Server/HubConfig/ChartHub.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using RealTimeCharts_Server.Models;
+
+namespace RealTimeCharts_Server.HubConfig
+{
+  public class ChartHub : Hub
+  {
+    public async Task BroadcastChartData(List<ChartModel> data) => await Clients.All.SendAsync("broadcastchartdata", data);
+  }
+}
